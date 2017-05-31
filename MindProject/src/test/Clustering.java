c81b0dead238e5690eceb38fe38ba3e43e0b295f
tomasdani <danielaftomas@gmail.com>
2017-05-31 10:37:24 +0100

    first commit

---
+++ MindProject/src/test/Clustering.java
@@ -0,0 +1,105 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Clustering {
+
+	public static void main(String[] args) {
+System.out.println("raios");
+		
+		ArrayList<Integer> aNumb = new ArrayList<Integer>();
+		int nClusters;
+		
+		System.out.println("ini size: " + aNumb.size());
+		
+		readArray(aNumb);
+		System.out.println("Final array size" + aNumb.size());
+		System.out.println("Array:" + aNumb);
+		
+		System.out.println("Insira o numero de Clusters:");		
+		
+		do{
+		nClusters=readInt();
+		
+		if(nClusters>aNumb.size()){
+			System.out.println("Must insert a number smaller then the array's size: "+ aNumb.size());
+		}
+		}while(nClusters>aNumb.size());
+	
+
+		KmeansAlg.cenas(aNumb, nClusters);
+			
+	}
+	
+
+	
+	//method to read an array of integers
+	public static void readArray(ArrayList<Integer> array) {
+		Scanner a;
+		int x = 0;
+		String st ="";
+		String stopSign="end";
+		boolean badN=true;
+		System.out.println("insert numbers");
+		do {
+			a = new Scanner(System.in);
+			if(a.hasNextInt()){
+				try{
+					x=Integer.parseInt(a.nextLine());
+				}catch(NumberFormatException e){
+					System.out.println("Char not valid. Insert only numbers numbers.");	
+					continue;
+				}
+				array.add(x);
+				System.out.println("+1 added");
+				badN=false;
+			}
+			else  {
+				st = a.nextLine();
+				if(st.equals(stopSign)){
+					if(!array.isEmpty()){
+					System.out.println("end of array");
+					badN=false;
+					break;
+					}
+					else{
+						System.out.println("the Array is still empty, please insert numbers.");
+						continue;
+					}
+				}
+				else{
+					System.out.println("Char not valid. Choose an Integer.");
+				}
+			}
+		} while (!st.equals(stopSign) || badN );
+
+	}
+	
+	//method to read one Int
+	public static int readInt(){
+		Scanner b;
+		int x = 0;
+		boolean badN=true;
+		do {
+			b = new Scanner(System.in);
+			if(!b.hasNextInt()){
+				System.out.println("Char not valid. Choose a number.");
+				continue;	
+			}
+			else  {
+				try{
+					x=Integer.parseInt(b.nextLine());
+				}catch(NumberFormatException e){
+					System.out.println("Char not valid. Insert only numbers numbers.");	
+					continue;
+				}
+					badN=false;
+			}
+		} while (badN);
+		//b.close();
+		return x;
+	
+	}
+
+}
---
+++ MindProject/src/test/KmeansAlg.java
@@ -0,0 +1,183 @@
+package test;
+
+import java.util.ArrayList;
+
+public class KmeansAlg {
+	static int d[]; // array w/ nº of the arrayL
+	static int k[][]; // matriz w/ solution
+	static int tempk[][]; // matriz w/ solution
+	static double m[]; // array w/ size of clusters asked, mean!
+	static double diff[]; // distances 
+
+	public static void cenas(ArrayList a, int p) {
+
+		/* Initialising arrays */
+
+		int n = a.size();//size of the array
+		//turn arrayL to array 
+		d=new int[n];
+		for (int i=0; i<a.size(); i++){
+			d[i]= (int) a.get(i);
+		}
+		
+		k = new int[p][n]; //solution
+		tempk = new int[p][n]; // temp solution
+		m = new double[p]; //means
+		diff = new double[p]; //diferences
+		int count[] = new int[p]; // column position in the solution K
+		/* Initializing m */
+		for (int i = 0; i < p; ++i)
+			m[i] = d[i];
+
+		int temp = 0;
+		int flag = 0;
+		do {
+			for (int i = 0; i < p; ++i)
+				for (int j = 0; j < n; ++j) {
+					k[i][j] = -1;
+				}
+			for (int i = 0; i < n; ++i) // for loop will cal cal_diff(int) for
+										// every element.
+			{
+				temp = cal_diff(d[i], p);
+
+				k[temp][count[temp]++] = d[i];
+			}
+			 cal_mean(p, n); // call to method which will calculate mean at this
+			// step.
+			 flag=check1(p,n); // check if terminating condition is satisfied.
+			if (flag != 1)
+				/*
+				 * Take backup of k in tempk so that you can check for
+				 * equivalence in next step
+				 */
+				for (int i = 0; i < p; ++i)
+					for (int j = 0; j < n; ++j)
+						tempk[i][j] = k[i][j];
+
+			System.out.println("\n\nAt this step");
+			System.out.println("\nValue of clusters");
+			for (int i = 0; i < p; ++i) {
+				System.out.print("K" + (i + 1) + "{ ");
+				for (int j = 0; k[i][j] != -1 && j < n - 1; ++j)
+					System.out.print(k[i][j] + " ");
+				System.out.println("}");
+			} // end of for loop
+			System.out.println("\nValue of m ");
+			for (int i = 0; i < p; ++i)
+				System.out.print("m" + (i + 1) + "=" + m[i] + "  ");
+
+			for (int t = 0; t < count.length; t++) {
+				count[t] = 0;
+			}
+
+		} while (flag == 0);
+
+		System.out.println("\n\n\nThe Final Clusters By Kmeans are as follows: ");
+		for (int i = 0; i < p; ++i) {
+			System.out.print("K" + (i + 1) + "{ ");
+			for (int j = 0; k[i][j] != -1 && j < n - 1; ++j)
+				System.out.print(k[i][j] + " ");
+			System.out.println("}");
+		}
+	}
+
+	
+	
+	
+	// This method will determine the cluster in which an element go at a
+	// particular step.
+	static int cal_diff(int a, int p) {
+		int temp1 = 0;
+		for (int i = 0; i < p; ++i) {
+			if (a > m[i])
+				diff[i] = a - m[i];
+			else
+				diff[i] = m[i] - a;
+		}
+		int val = 0;
+		double temp = diff[0];
+		for (int i = 0; i < p; ++i) {
+			if (diff[i] < temp) {
+				temp = diff[i];
+				val = i;
+			}
+		} // end of for loop
+		return val;
+	}
+
+	static void cal_mean(int p, int n) // This method will determine
+										// intermediate mean values
+	{
+		for (int i = 0; i < p; ++i)
+			m[i] = 0; // initializing means to 0
+		int cnt = 0;
+		for (int i = 0; i < p; ++i) {
+			cnt = 0;
+			for (int j = 0; j < n - 1; ++j) {
+				if (k[i][j] != -1) {
+					m[i] += k[i][j];
+					++cnt;
+				}
+			}
+			m[i] = m[i] / cnt;
+		}
+	}
+
+	static int check1(int p, int n) // This checks if previous k ie. tempk and
+									// current k are same.Used as terminating
+									// case.
+	{
+		for (int i = 0; i < p; ++i)
+			for (int j = 0; j < n; ++j)
+				if (tempk[i][j] != k[i][j]) {
+					return 0;
+				}
+		return 1;
+	}
+}
+
+/*
+Enter the number of elements
+8
+Enter 8 elements:
+2 3 6 8 12 15 18 22
+Enter the number of clusters:
+3
+
+At this step
+Value of clusters
+K1{ 2 }
+K2{ 3 }
+K3{ 6 8 12 15 18 22 }
+Value of m
+m1=2.0  m2=3.0  m3=13.5
+
+At this step
+Value of clusters
+K1{ 2 }
+K2{ 3 6 8 }
+K3{ 12 15 18 22 }
+Value of m
+m1=2.0  m2=5.666666666666667  m3=16.75
+
+At this step
+Value of clusters
+K1{ 2 3 }
+K2{ 6 8 }
+K3{ 12 15 18 22 }
+Value of m
+m1=2.5  m2=7.0  m3=16.75
+
+At this step
+Value of clusters
+K1{ 2 3 }
+K2{ 6 8 }
+K3{ 12 15 18 22 }
+Value of m
+m1=2.5  m2=7.0  m3=16.75
+
+The Final Clusters By Kmeans are as follows:
+K1{ 2 3 }
+K2{ 6 8 }
+K3{ 12 15 18 22 } */
