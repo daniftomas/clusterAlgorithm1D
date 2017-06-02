07befb20a5e0ef8faa7bbf034bbce693669f326e
tomasdani <danielaftomas@gmail.com>
2017-06-02 09:32:41 +0100

    d.2.6.932

--- MindProject/src/test/KmeansAlg.java
+++ MindProject/src/test/KmeansAlg.java
@@ -7,9 +7,8 @@ public class KmeansAlg {
 	static int k[][]; // matriz w/ solution
 	static int tempk[][]; // matriz w/ temp solution
 	static double m[]; // array w/ size of clusters asked, means
-	static double diff[]; // distances 
+	static double diff[]; // distances
 
-	
 	/**
 	 * method to group integers considering their distance
 	 * @param a arrayList that contains the inserted numbers
@@ -19,19 +18,19 @@ public class KmeansAlg {
 
 		/* Initialising arrays */
 
-		int n = a.size();//size of the array
-		//turn arrayL to array 
-		d=new int[n];
-		for (int i=0; i<a.size(); i++){
-			d[i]= (int) a.get(i);
+		int n = a.size();// size of the array
+		// turn arrayL to array
+		d = new int[n];
+		for (int i = 0; i < a.size(); i++) {
+			d[i] = (int) a.get(i);
 		}
-		
-		k = new int[p][n]; //solution
+
+		k = new int[p][n]; // solution
 		tempk = new int[p][n]; // temp solution
-		m = new double[p]; //means
-		diff = new double[p]; //diferences
+		m = new double[p]; // means
+		diff = new double[p]; // diferences
 		int count[] = new int[p]; // column position in the solution K
-		
+
 		/* Initializing m */
 		for (int i = 0; i < p; ++i)
 			m[i] = d[i];
@@ -39,39 +38,41 @@ public class KmeansAlg {
 		int temp = 0;
 		int flag = 0;
 		do {
-			//create matriz w/ -1 elements
+			// create matriz w/ -1 elements
 			for (int i = 0; i < p; ++i)
 				for (int j = 0; j < n; ++j) {
 					k[i][j] = -1;
 				}
-			// for loop will cal cal_diff(int) for  every element.
-			for (int i = 0; i < n; ++i) 
-			{
+			// for loop will cal cal_diff(int) for every element.
+			for (int i = 0; i < n; ++i) {
 				temp = cal_diff(d[i], p);
 
 				k[temp][count[temp]++] = d[i];
 			}
-			
+
 			// call to method which will calculate mean at this step.
-			 cal_mean(p, n); 
-			 
+			cal_mean(p, n);
+
 			// check if terminating condition is satisfied.
-			 flag=check1(p,n); 
+			flag = check1(p, n);
 			if (flag != 1)
-				// Take backup of k in tempk so that it can be checked for equivalence in next step
+				// Take backup of k in tempk so that it can be checked for
+				// equivalence in next step
 				for (int i = 0; i < p; ++i)
 					for (int j = 0; j < n; ++j)
 						tempk[i][j] = k[i][j];
 
 			System.out.println("\n\nAt this step");
 			System.out.println("\nValue of clusters");
+			
 			for (int i = 0; i < p; ++i) {
 				System.out.print("K" + (i + 1) + "{ ");
 				for (int j = 0; k[i][j] != -1 && j < n - 1; ++j)
 					System.out.print(k[i][j] + " ");
 				System.out.println("}");
-			} 
+			}
 			System.out.println("\nValue of m ");
+			
 			for (int i = 0; i < p; ++i)
 				System.out.print("m" + (i + 1) + "=" + m[i] + "  ");
 
@@ -90,23 +91,19 @@ public class KmeansAlg {
 		}
 	}
 
-	
 	/**
 	 * This method will determine the cluster in which an element go at a particular step.
 	 * @param a element i of the array
 	 * @param p number of clusters requested
-	 * @return the [i], that represents a cluster,  in which the element will go
+	 * @return the [i], that represents a cluster, in which the element will go
 	 */
 	static int cal_diff(int a, int p) {
 		int temp1 = 0;
 		for (int i = 0; i < p; ++i) {
-			diff[i] =  Math.abs(a - m[i]);
+			diff[i] = Math.abs(a - m[i]);
 			/*
-			if (a > m[i])
-				diff[i] = a - m[i];
-			else
-				diff[i] = m[i] - a;
-				*/
+			 * if (a > m[i]) diff[i] = a - m[i]; else diff[i] = m[i] - a;
+			 */
 		}
 		int val = 0;
 		double temp = diff[0];
@@ -115,17 +112,16 @@ public class KmeansAlg {
 				temp = diff[i];
 				val = i;
 			}
-		} // end of for loop
+		} 
 		return val;
 	}
-	
-/**
- *  This method will determine intermediate mean values
- * @param p number of clusters requested
- * @param n number of elements  of the array inserted
- */
-	static void cal_mean(int p, int n) 
-	{
+
+	/**
+	 * This method will determine intermediate mean values
+	 * @param p number of clusters requested
+	 * @param n number of elements of the array inserted
+	 */
+	static void cal_mean(int p, int n) {
 		for (int i = 0; i < p; ++i)
 			m[i] = 0; // initializing means to 0
 		int cnt = 0;
@@ -142,13 +138,13 @@ public class KmeansAlg {
 	}
 
 	/**
-	 * // This checks if previous k( tempk) and current k are same.Used as terminating  case. Validates solution.
+	 * // This checks if previous k( tempk) and current k are same.Used as
+	 * terminating case. Validates solution.
 	 * @param p number of clusters requested
 	 * @param n number of elements of the array inserted
 	 * @return 1 if the tempK is equal to the solution, 0 if its not.
 	 */
-	static int check1(int p, int n) 
-	{
+	static int check1(int p, int n) {
 		for (int i = 0; i < p; ++i)
 			for (int j = 0; j < n; ++j)
 				if (tempk[i][j] != k[i][j]) {
